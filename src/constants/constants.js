c24232f0bf62e62506f22ec6dd53bbadd21b77bc
DanielC19 <90290136+DanielC19@users.noreply.github.com>
2024-05-15 15:11:52 -0500

    Update API URLs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,21 @@
 
-export const API_URL_TODO = 'http://localhost:8000/api/todo/';
+export const API_URL_TODO = 'http://34.29.206.12/api/todo/';
 
-export const API_URL_SIMULATE = 'http://localhost:8000/api/simulate/';
+export const API_URL_SIMULATE = 'http://34.29.206.12/api/simulate/';
 
-export const API_URL_STREAK = 'http://localhost:8000/api/streak/';
+export const API_URL_STREAK = 'http://34.29.206.12/api/streak/';
 
-export const API_URL_LOGIN = 'http://localhost:8000/api/login/';
+export const API_URL_LOGIN = 'http://34.29.206.12/api/login/';
 
-export const API_URL_REGISTER = 'http://localhost:8000/api/register/';
+export const API_URL_REGISTER = 'http://34.29.206.12/api/register/';
 
-export const API_URL_LOGOUT = 'http://localhost:8000/api/logout/';
+export const API_URL_LOGOUT = 'http://34.29.206.12/api/logout/';
 
-export const API_URL_TOKEN_REFRESH = 'http://localhost:8000/api/token/refresh/';
+export const API_URL_TOKEN_REFRESH = 'http://34.29.206.12/api/token/refresh/';
 
-export const API_URL_ANALYTICS = 'http://localhost:8000/api/analytics/';
+export const API_URL_ANALYTICS = 'http://34.29.206.12/api/analytics/';
 
-export const API_URL_CALENDAR = 'http://localhost:8000/api/calendar/';
+export const API_URL_CALENDAR = 'http://34.29.206.12/api/calendar/';
 
 export const DEFAULT_HABIT = {
   name: '',
