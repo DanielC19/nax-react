cab8ea9e63c98ecda790fb9bbf1d633b7e559aa4
DanielC19 <90290136+DanielC19@users.noreply.github.com>
2024-05-15 23:54:58 -0500

    New API URLs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,21 @@
 
-export const API_URL_TODO = 'http://34.29.206.12/api/todo/';
+export const API_URL_TODO = 'http://34.170.77.201/api/todo/';
 
-export const API_URL_SIMULATE = 'http://34.29.206.12/api/simulate/';
+export const API_URL_SIMULATE = 'http://34.170.77.201/api/simulate/';
 
-export const API_URL_STREAK = 'http://34.29.206.12/api/streak/';
+export const API_URL_STREAK = 'http://34.170.77.201/api/streak/';
 
-export const API_URL_LOGIN = 'http://34.29.206.12/api/login/';
+export const API_URL_LOGIN = 'http://34.170.77.201/api/login/';
 
-export const API_URL_REGISTER = 'http://34.29.206.12/api/register/';
+export const API_URL_REGISTER = 'http://34.170.77.201/api/register/';
 
-export const API_URL_LOGOUT = 'http://34.29.206.12/api/logout/';
+export const API_URL_LOGOUT = 'http://34.170.77.201/api/logout/';
 
-export const API_URL_TOKEN_REFRESH = 'http://34.29.206.12/api/token/refresh/';
+export const API_URL_TOKEN_REFRESH = 'http://34.170.77.201/api/token/refresh/';
 
-export const API_URL_ANALYTICS = 'http://34.29.206.12/api/analytics/';
+export const API_URL_ANALYTICS = 'http://34.170.77.201/api/analytics/';
 
-export const API_URL_CALENDAR = 'http://34.29.206.12/api/calendar/';
+export const API_URL_CALENDAR = 'http://34.170.77.201/api/calendar/';
 
 export const DEFAULT_HABIT = {
   name: '',
